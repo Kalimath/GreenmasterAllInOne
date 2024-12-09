a6877a9e4287096af22336e549d436c4fafbb1f8
Mathieu Broeckhoven <mathieu.broeckhoven@protime.eu>
2024-12-09 13:01:24 +0100

    all crud methods from demo added

--- Greenmaster.Api/Controllers/BloomController.cs
+++ Greenmaster.Api/Controllers/BloomController.cs
@@ -1,5 +1,8 @@
 using Greenmaster.Application.Features.Blooms.Commands.CreateBloomCommand;
+using Greenmaster.Application.Features.Blooms.Commands.DeleteBloomCommand;
+using Greenmaster.Application.Features.Blooms.Commands.UpdateBloomCommand;
 using Greenmaster.Application.Features.Blooms.Dto;
+using Greenmaster.Application.Features.Blooms.Queries.GetBloomDetailQuery;
 using Greenmaster.Application.Features.Blooms.Queries.GetBloomsQuery;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
@@ -20,9 +23,39 @@ public class BloomController(IMediator mediatr) : ControllerBase
     }
 
     [HttpPost(Name = "CreateBloom")]
+    [ProducesResponseType(Status202Accepted)] 
+    [ProducesResponseType(Status400BadRequest)]
     public async Task<ActionResult<BloomDetailDto>> Create([FromBody] CreateBloomCommand createBloomCommand)
     {
         var response = await mediatr.Send(createBloomCommand);
-        return Ok(response);
+        return Accepted(response);
+    }
+
+    [HttpGet("{id}", Name = "GetBloomById")]
+    [ProducesResponseType(Status200OK)] 
+    [ProducesResponseType(Status404NotFound)]
+    public async Task<ActionResult<BloomDetailDto>> GetById(Guid id)
+    {
+        var getBloomDetailQuery = new GetBloomDetailQuery{Id = id};
+        return Ok(await mediatr.Send(getBloomDetailQuery));
+    }
+
+    [HttpPut(Name = "UpdateBloom")]
+    [ProducesResponseType(Status204NoContent)]
+    [ProducesResponseType(Status400BadRequest)]
+    public async Task<IActionResult> Update([FromBody] UpdateBloomCommand updateBloomCommand)
+    {
+        await mediatr.Send(updateBloomCommand); //TODO: may not be fully implemented
+        return NoContent();
+    }
+
+    [HttpDelete("{id}", Name = "DeleteBloom")]
+    [ProducesResponseType(Status204NoContent)]
+    [ProducesResponseType(Status404NotFound)]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        var deleteBloomCommand = new DeleteBloomCommand{Id = id};
+        await mediatr.Send(deleteBloomCommand); //TODO: may not be fully implemented
+        return NoContent();
     }
 }
